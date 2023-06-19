3fb820167d2b61e55e84064393cce199be566ab4
poiuy98749 <poiuy98749@naver.com>
2023-06-19 19:41:04 +0900

    Partial implementation of style recognition model

---
+++ Modeling/FashionModel/test.py
@@ -0,0 +1,6 @@
+from fashion.models import build_predictor
+from mmcv import Config
+
+cfg = Config.fromfile('./configs/attribute_predict_coarse/roi_predictor_vgg_attr.py')
+model = build_predictor(cfg.model)
+breakpoint()
