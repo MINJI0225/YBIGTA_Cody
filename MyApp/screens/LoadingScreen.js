58d78d8b4dccdb31f89cd30cfb1536cbb6669253
poiuy98749 <poiuy98749@naver.com>
2023-08-09 02:31:41 +0900

    Fix: Loading / signup screen

--- MyApp/screens/LoadingScreen.js
+++ MyApp/screens/LoadingScreen.js
@@ -1,45 +1,21 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { View, ActivityIndicator, StyleSheet, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, ActivityIndicator, Text } from 'react-native';
 import Separator from '../components/Separator.js';
 
-export default function LoadingScreen({ navigation }) {
+export default function LoadingScreen() {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState(null); // 데이터 상태 추가
+  const [data, setData] = useState(null); // Add data state
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/userStyle/get'); // 모델을 돌리고 응답을 받는 API 엔드포인트 URL로 수정해야함
-        if (response.ok) {
-          console.log('모델 응답 성공');
-          const data = await response.json();
-          console.log(data);
-          setIsLoading(false);
-          setData(data); // 데이터 설정
-          navigation.navigate('StyleIcon', { data });
-          // 응답을 처리한 후에 캐릭터 띄우는 화면으로 데이터와 함께 이동
-        } else {
-          console.log('모델 응답 실패');
-          setIsLoading(false);
-          // 에러 처리
-        }
-      } catch (error) {
-        console.log('Error:', error);
-        setIsLoading(false);
-        // 에러 처리
-      }
-    };
-    fetchData(); // 데이터 로딩 함수 호출
+    // 이곳에 로딩시 수행할 작업들을 넣어주세요.
   }, []);
 
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color="#AFD3E2" />
-        <Separator />
-        <Text>Loading...</Text>
-      </View>
-    )
-  }
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#AFD3E2" />
+      <Separator />
+      <Text>Loading...</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
