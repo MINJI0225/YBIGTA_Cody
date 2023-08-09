5750117ef2c0ef9add08541106c0ebee398d7ecb
poiuy98749 <poiuy98749@naver.com>
2023-08-10 00:29:58 +0900

    Fix: Create checkpoint path

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 # Download best.pth from google drive
 
 pip install gdown
+
+# Create directory if not exist
+mkdir -p ./Modeling/FashionModel/checkpoints
 gdown https://drive.google.com/uc?id=1Eg5yPfh_YgaXWWTnB3M_baas25_olfaG -O ./Modeling/FashionModel/checkpoints/best.pth
